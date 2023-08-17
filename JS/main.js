7bc03fa7ad982ff41b18a635b74b80b8e23235d0
Natsuki Miura <n.miura.com>
2023-08-17 16:27:44 +0900

    大項目の取得、描画OK

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,13 @@
 import textGroup from "./item.js";
 
-console.log(textGroup[0][0][0][0])
+const accordionArea = document.getElementById('accordion-area');
+
+for (let i = 0; i < textGroup.length ; i++){
+    const listItem = document.createElement("div")
+    listItem.textContent = textGroup[i][0][0][0];
+    accordionArea.appendChild(listItem);
+  }
+
+  
+
+// console.log(textGroup[0][0][0][0],textGroup[1][0][0][0],textGroup[2][0][0][0])
