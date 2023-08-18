155f17f0a0e4628ec5aa4c2664f6abc6fe165961
Natsuki Miura <n.miura.com>
2023-08-18 14:11:40 +0900

    大項目のfor文に中項目のfor文をネストして取得

--- JS/main.js
+++ JS/main.js
@@ -2,12 +2,28 @@ import textGroup from "./item.js";
 
 const accordionArea = document.getElementById('accordion-area');
 
-for (let i = 0; i < textGroup.length ; i++){
-    const listItem = document.createElement("div")
-    listItem.textContent = textGroup[i][0][0][0];
-    accordionArea.appendChild(listItem);
-  }
 
-  
+// 大項目を取得するfor文の中で中項目のfor文でネスト
 
-// console.log(textGroup[0][0][0][0],textGroup[1][0][0][0],textGroup[2][0][0][0])
+for (let i = 0; i < textGroup.length; i++) {
+    const largeItem = document.createElement("div")
+    largeItem.id = 'largeItem' + i;
+    largeItem.textContent = textGroup[i][0][0][0];
+    accordionArea.appendChild(largeItem);
+
+    for (let j = 0; j < textGroup[i].length; j++) {
+        const middleItem = document.createElement("div")
+        middleItem.id = 'middleItem' + i;
+        middleItem.textContent = textGroup[i][j][0][1];
+        largeItem.appendChild(middleItem);
+
+        
+
+    };
+}
+
+//   function openMiddleItem() {
+
+//   };
+
+
